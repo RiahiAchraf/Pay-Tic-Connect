d1d29052d937d95a2a63dc90613b2cd025e07489
RiahiAchraf <riahiachraf1@gmail.com>
2023-08-11 06:12:12 +0100

    feat: create a static data

---
+++ src/utils/data.ts
@@ -0,0 +1,47 @@
+const data = [
+  {
+    id: 1,
+    status: 'InProgress',
+    title: 'Human Interest Form',
+    content: 'Fill out human interest distribution form',
+  },
+  {
+    id: 2,
+    status: 'InProgress',
+    title: 'Purchase present',
+    content: 'Get an anniversary gift',
+  },
+  {
+    id: 3,
+    status: 'InProgress',
+    title: 'Invest in investments',
+    content: 'Call the bank to talk about investments',
+  },
+  {
+    id: 4,
+    status: 'InProgress',
+    title: 'Daily reading',
+    content: 'Finish reading Intro to UI/UX',
+  },
+];
+
+const statuses = [
+  // {
+  //   status: "open",
+  //   color: "#EB5A46",
+  // },
+  {
+    status: 'In Progress',
+    color: '#00C2E0',
+  },
+  // {
+  //   status: "in review",
+  //   color: "#C377E0",
+  // },
+  {
+    status: 'DONE',
+    color: '#3981DE',
+  },
+];
+
+export { data, statuses };
